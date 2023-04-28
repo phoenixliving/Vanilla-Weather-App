02b8687900f8d8700d676eff101895786b1bca24
phoenixliving <89965692+phoenixliving@users.noreply.github.com>
2023-04-28 15:11:54 +1000

    added fahrenheit back to celsius conversion

--- src/app.js
+++ src/app.js
@@ -69,6 +69,13 @@ function displayFahrenheitTemperature(event){
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
+function displayCelsiusTemperature (event){
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+
+}
+
 let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
@@ -76,5 +83,8 @@ form.addEventListener("submit", handleSubmit);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
