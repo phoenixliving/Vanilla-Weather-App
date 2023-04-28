4a3c829a4e32551537b4550efe0b9a98cccc7f7b
phoenixliving <89965692+phoenixliving@users.noreply.github.com>
2023-04-28 15:23:01 +1000

    added active status for units display

--- src/app.js
+++ src/app.js
@@ -64,14 +64,20 @@ function handleSubmit(event){
 
 function displayFahrenheitTemperature(event){
   event.preventDefault();
-  let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
   let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
 function displayCelsiusTemperature (event){
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 
 }
