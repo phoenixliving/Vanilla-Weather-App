425b9c8415319bf17995b5fde0e7bc9988f4488a
phoenixliving <89965692+phoenixliving@users.noreply.github.com>
2023-05-09 16:58:24 +1000

    Updated forecast display
    updated forecast days with forecast variable, formatted date stamp, added rounding to forecast, added index to forecast.for Each function to limit day display

--- src/app.js
+++ src/app.js
@@ -24,26 +24,39 @@ return`${day} ${hours} ${minutes}`;
 }
 
 
+function formatDay(timestamp){
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+
+  return days[day];
+}
+
 function displayForecast(response){
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Thu", "Fri", "Sat"];
+  // let days = ["Thu", "Fri", "Sat"];
+
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function(day){
+  forecast.forEach(function(forecastDay, index){
+
+    if (index <6){
 
   forecastHTML = forecastHTML +
   `
     <div class="col-2">
-      <div class="weather-forecast-date">${day}</div>
-          <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="" width="50" class="weather-forecast-image" />
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+          <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="50" class="weather-forecast-image" />
         <div class="weather-forecast-temperature">
-            <span class="weather-forecast-temp-max">18</span>
-            <span class="weather-forecast-temp-min"> 12</span>
+            <span class="weather-forecast-temp-max">${Math.round(forecastDay.temp.max)}°</span>
+            <span class="weather-forecast-temp-min">${Math.round(forecastDay.temp.min)}°</span>
       </div>
       </div>
 `;
-
+}
   })
 
 forecastHTML = forecastHTML + `</div>`;
@@ -52,14 +65,15 @@ forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates){
-
+console.log(coordinates);
 let apiKey = "57b2c40fdae71a6ba41d72685e3226e2";
 let units = "metric";
 
-let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
 
 console.log(apiUrl);
-axios.get(apiUrl). then(displayForecast);
+
+axios.get(apiUrl).then(displayForecast);
 };
 
 
